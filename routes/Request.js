89f07c7633e898d207ff08aa8e2ef4bb2f53a013
Yuvraj <yuvraj1mann@gmail.com>
2021-11-12 18:41:38 +0530

    work done

--- routes/Request.js
+++ routes/Request.js
@@ -3,72 +3,81 @@ var router = express.Router();
 var User = require("../models/User.js");
 var authenticate = require("../authenticate");
 var Request = require("../models/Request");
-var moment=require('moment');
+var moment = require("moment");
 
 router.get("/getRequest", authenticate.verifyUser, (req, res, next) => {
-  let usr=req.user;
+  let usr = req.user;
 
-  if(usr.user_type.toString()=="police_user"){
-    
+  if (usr.user_type.toString() == "police_user") {
+    Request.find({ police: usr._id })
+      .populate("victim")
+      .populate("police")
+      .then((resp) => {
+        let date, time;
+        let f_arr = [];
 
-    Request.find({police:usr._id}).populate('victim').populate('police')
-    .then((resp) => {
-      let date,time;
-      let f_arr=[];
+        for (let i = 0; i < resp.length; ++i) {
+          let obj = resp[i];
+          const clone = JSON.parse(JSON.stringify(obj));
+          clone["date"] = moment(resp[i].createdAt).format("DD/MM/YYYY");
+          clone["time"] = moment(resp[i].createdAt).format("hh:mm");
+          console.log(clone);
+          f_arr.push(clone);
+        }
 
-      for(let i=0;i<resp.length;++i){
-          f_arr.push(resp[i]);
-          f_arr[i]['date']=moment(f_arr[i].createdAt).format("DD/MM/YYYY");
-          f_arr[i]['time']=moment(f_arr[i].createdAt).format('hh:mm');     
-      }
-
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.json({
-        success: true,
-        status: f_arr,
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json({
+          success: true,
+          status: f_arr,
+        });
+      })
+      .catch((err) => {
+        next(err);
       });
-    })
-    .catch((err) => {
-      next(err);
-    });
-  }
-  else if(usr.user_type.toString()=="normal_user"){
-    Request.find({victim:usr._id}).populate('victim').populate('police')
-    .then((resp) => {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.json({
-        success: true,
-        status: resp,
+  } else if (usr.user_type.toString() == "normal_user") {
+    Request.find({ victim: usr._id })
+      .populate("victim")
+      .populate("police")
+      .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json({
+          success: true,
+          status: resp,
+        });
+      })
+      .catch((err) => {
+        next(err);
       });
-    })
-    .catch((err) => {
-      next(err);
-    });
   }
-    
 });
 
-router.post('/changeStatus/:requestId',authenticate.verifyUser,(req,res,next)=>{
-    Request.find({id:req.params.requestId}).then((request)=>{
-        request['status']=req.body.status;
-        request.save().then((resp)=>{
+router.post(
+  "/changeStatus/:requestId",
+  authenticate.verifyUser,
+  (req, res, next) => {
+    Request.find({ id: req.params.requestId })
+      .then((request) => {
+        request["status"] = req.body.status;
+        request
+          .save()
+          .then((resp) => {
             res.statusCode = 200;
             res.setHeader("Content-Type", "application/json");
             res.json({
-                success: true,
-                status: resp,
+              success: true,
+              status: resp,
             });
-        })
-        .catch((err)=>{
+          })
+          .catch((err) => {
             next(err);
-        });
-    })
-    .catch((err)=>{
+          });
+      })
+      .catch((err) => {
         next(err);
-    });
-})
+      });
+  }
+);
 
-
-module.exports=router;
+module.exports = router;
